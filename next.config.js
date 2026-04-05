4da51d912933e6a6980d59b7e33e356c7da0735c
adriansga <adriansga718@gmail.com>
2026-04-05 11:34:19 +0200

    Fix: Add turbopack config, remove duplicate next.config.ts, fix eslint-config-next version

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Twoja konfiguracja
+  turbopack: {},
 };
 
 module.exports = withPWA(nextConfig);
